b6a53b3e7d67d1fb2a2a3c408e1336b68c31a8b7
Priyanshu <80645068+PriyanshuQ@users.noreply.github.com>
2023-06-11 19:16:21 +0530

    Time: 2 ms (99.15%), Space: 43.7 MB (8.07%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -6,8 +6,8 @@ class Solution {
 
         int carry = 0;
         while(l1!=null || l2!=null){
-            int l1_val = (l1!=null)?l1.val : 0;
-            int l2_val = (l2!=null)?l2.val : 0;
+            int l1_val = (l1!=null)?l1.val:0;
+            int l2_val = (l2!=null)?l2.val:0;
 
             int current_sum = l1_val+l2_val+carry;
             carry = current_sum/10;
@@ -18,7 +18,7 @@ class Solution {
 
             if(l1!=null) l1=l1.next;
             if(l2!=null) l2=l2.next;
-            l3 = l3.next;
+            l3=l3.next;
         }
 
         if(carry>0){
@@ -26,6 +26,7 @@ class Solution {
             l3.next = new_node;
             l3 = l3.next;
         }
+
         return head.next;
     }
 }
