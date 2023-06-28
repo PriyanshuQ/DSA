09c37fa8442763ce380ed1896f4bf2ca5c0b97e1
Priyanshu <80645068+PriyanshuQ@users.noreply.github.com>
2023-06-28 09:47:14 +0530

    Time: 1 ms (99.34%), Space: 44.7 MB (37.51%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int jump(int[] nums) {
+        int current=0, farthest=0, jumps=0;
+        for(int i=0; i<nums.length-1; i++){
+            farthest= Math.max(farthest, nums[i]+i);
+            if(i==current){
+                current= farthest;
+                jumps++;
+            }
+        }
+        return jumps;
+    }
+}
+
+//When i is equal to current, it means that we have reached the maximum reachable position from the previous jump. This indicates that we need to make another jump to continue moving forward.
